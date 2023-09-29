6b5a6cae87ada1df53860f48f022e4fe21ff5ff4
ajay5776 <144900898+ajay5776@users.noreply.github.com>
2023-09-29 09:19:56 +0530

    setup

---
+++ assets/js/chart-custom.js
@@ -0,0 +1,463 @@
+
+// bebgin line chart display
+var lineChart = document.getElementById("line-chart2").getContext('2d')
+var lineChart2 = document.getElementById("line-chart").getContext('2d')
+
+
+// line chart options
+var options = {
+    borderWidth: 2,
+    cubicInterpolationMode: 'monotone', // make the line curvy over zigzag
+    pointRadius: 2,
+    pointHoverRadius: 5,
+    pointHoverBackgroundColor: '#fff',
+    pointHoverBorderWidth: 4,
+
+
+}
+
+// create linear gradients for line chart
+var gradientOne = lineChart.createLinearGradient(0, 0, 0, lineChart.canvas.clientHeight)
+gradientOne.addColorStop(0, 'rgba(51, 169, 247, 0)')
+gradientOne.addColorStop(1, 'rgba(0, 0, 0, 0)')
+
+var gradientTwo = lineChart.createLinearGradient(0, 0, 0, lineChart.canvas.clientHeight)
+gradientTwo.addColorStop(0, 'rgba(195, 113, 239, 0)')
+gradientTwo.addColorStop(1, 'rgba(0, 0, 0, 0)')
+
+
+new Chart(
+    lineChart,
+    {
+        type: 'line',
+        data: {
+            labels: ['2h', '6h', '8h', '10h', '12h', '16h', '18h', '20h', '22h'],
+            datasets: [
+                {
+                    // label: 'Spending',
+                    data: [200, 250, 300, 350, 290, 360, 310, 390, 400],
+                    ...options,
+                    borderColor: '#916BFF',
+                    fill: 'start',
+                    backgroundColor: gradientTwo
+                },
+                {
+                    // label: 'Emergency',
+                    data: [50, 70, 40, 100, 200, 100, 170, 150, 190],
+                    ...options,
+                    borderColor: '#4599E5',
+                    fill: 'start',
+                    backgroundColor: gradientOne
+                },
+                {
+                    // label: 'redzone',
+                    data: [240, 230, 250, 220, 100, 190, 50, 100, 60],
+                    ...options,
+                    borderColor: '#EA4335',
+                    fill: 'start',
+                    backgroundColor: gradientTwo
+                },
+                {
+                    // label: 'highzone',
+                    data: [500, 480, 420, 450, 400, 470, 410, 350, 340],
+                    ...options,
+                    borderColor: '#4599E5',
+                    fill: 'start',
+                    backgroundColor: gradientOne
+                }
+            ]
+        },
+        options: {
+            responsive: true,
+            maintainAspectRatio: false,
+            plugins: {
+                legend: {
+                    display: false, // hide display data about the dataset
+                },
+                tooltip: { // modify graph tooltip
+                    backgroundColor: 'rgba(53, 27, 92, 0.8)',
+                    caretPadding: 5,
+                    boxWidth: 5,
+                    usePointStyle: 'triangle',
+                    boxPadding: 3
+                }
+            },
+            scales: {
+                x: {
+                    grid: {
+                        display: false // set display to false to hide the x-axis grid
+                    },
+                    beginAtZero: true
+                },
+                y: {
+                    ticks: {
+                        callback: function (value, index, values) {
+                            return ' ' + value // prefix '$' to the dataset values
+                        },
+                        stepSize: 250
+                    }
+                }
+            }
+        }
+    }
+)
+
+new Chart(
+    lineChart2,
+    {
+        type: 'line',
+        data: {
+            labels: ['2h', '6h', '8h', '10h', '12h', '16h', '18h', '20h', '22h'],
+            datasets: [
+                {
+                    label: 'Spending',
+                    data: [800, 1800, 1500, 1800, 2500, 2200, 1500, 2200, 2850],
+                    ...options,
+                    borderColor: '#916BFF',
+                    fill: 'start',
+                    backgroundColor: gradientTwo
+                },
+                {
+                    label: 'Emergency',
+                    data: [500, 550, 1150, 700, 1500, 1200, 550, 1000, 600],
+                    ...options,
+                    borderColor: '#4599E5',
+                    fill: 'start',
+                    backgroundColor: gradientOne
+                },
+                {
+                    label: 'redzone',
+                    data: [1000, 850, 600, 950, 500, 750, 900, 600, 850],
+                    ...options,
+                    borderColor: '#EA4335',
+                    fill: 'start',
+                    backgroundColor: gradientTwo
+                },
+                {
+                    label: 'highzone',
+                    data: [2850, 2800, 2600, 2900, 2000, 1400, 2500, 1900, 1600],
+                    ...options,
+                    borderColor: '#4599E5',
+                    fill: 'start',
+                    backgroundColor: gradientOne
+                }
+            ]
+        },
+        options: {
+            responsive: true,
+            maintainAspectRatio: false,
+            plugins: {
+                legend: {
+                    display: false, // hide display data about the dataset
+                },
+                tooltip: { // modify graph tooltip
+                    backgroundColor: 'rgba(53, 27, 92, 0.8)',
+                    caretPadding: 5,
+                    boxWidth: 5,
+                    usePointStyle: 'triangle',
+                    boxPadding: 3
+                }
+            },
+            scales: {
+                x: {
+                    grid: {
+                        display: false // set display to false to hide the x-axis grid
+                    },
+                    beginAtZero: true
+                },
+                y: {
+                    ticks: {
+                        callback: function (value, index, values) {
+                            return ' ' + value // prefix '$' to the dataset values
+                        },
+                        stepSize: 1000
+                    }
+                }
+            }
+        }
+    }
+)
+
+
+
+// ################ [ Count Chart ] #################
+var oilCanvas = document.getElementById("oilChart");
+
+// Chart.defaults.global.defaultFontFamily = "Lato";
+// Chart.defaults.global.defaultFontSize = 10;
+
+var oilData = {
+    // labels: [
+    //     "Saudi Arabia",
+    //     "Russia",
+    //     "Iraq",
+    //     "United Arab Emirates",
+    //     "Canada"
+    // ],
+    datasets: [
+        {
+            data: [150.3, 50.2, 30.2, 20.2],
+            borderColor: "#262626",
+            borderWidth: 4,
+            backgroundColor: [
+                "#4599E5",
+                "#BD00FF",
+                "#EA4335",
+                "#916BFF"
+            ]
+        }
+    ]
+};
+
+var pieChart = new Chart(oilCanvas, {
+    type: 'pie',
+    data: oilData,
+
+});
+
+// ################ [ rate Chart ] #################
+
+var ctx = document.getElementById("ratechart");
+var ratechart = new Chart(ctx, {
+    type: 'doughnut',
+    data: {
+        // labels: ['OK', 'WARNING', 'CRITICAL', 'UNKNOWN'],
+        datasets: [
+            {
+                label: 'Facebook',
+                data: [50, 50],
+                backgroundColor: [
+                    "#4599E5",
+                    "#5A5A5A",
+                ],
+                borderColor: "#262626",
+                borderWidth: 3
+            },
+            {
+                label: 'Tiktok',
+                data: [40, 50],
+                backgroundColor: [
+                    "#BD00FF",
+                    "#5A5A5A",
+                ],
+                borderColor: "#262626",
+                borderWidth: 3
+            },
+            {
+                label: 'Youtube',
+                data: [30, 50],
+                backgroundColor: [
+                    "#EA4335",
+                    "#5A5A5A",
+                ],
+                borderColor: "#262626",
+                borderWidth: 3
+            },
+            {
+                label: 'News',
+                data: [20, 50],
+                backgroundColor: [
+                    "#916BFF",
+                    "#5A5A5A",
+                ],
+                borderColor: "#262626",
+                borderWidth: 3
+            }
+        ]
+    },
+    options: {
+        //cutoutPercentage: 40,
+        responsive: true,
+
+    }
+});
+
+// ___________________________
+
+
+
+
+
+// ? %%%%%%%%%%%%%% stacked Chart  %%%%%%%%%%%%%%
+
+// var ctx = document.getElementById("barchart").getContext('2d');
+// var myChart = new Chart(ctx, {
+//     type: 'bar',
+//     data: {
+//         labels: ["2k", "4k", "6k", "8k", "10k", "12k", "14k", "16k", "18k", "20k"],
+//         datasets: [
+//             {
+//                 // label: 'cần sa',
+//                 backgroundColor: "#4599E5",
+//                 data: [5000, 4500, 4000, 3500, 3000, 2500, 2000, 1500, 1000, 500],
+//             },
+//             {
+//                 // label: 'bóng cười',
+//                 backgroundColor: "#BD00FF",
+//                 data: [5000, 4500, 4000, 3500, 3000, 2500, 2000, 1500, 1000, 500],
+//             },
+//             {
+//                 // label: 'ma tuý',
+//                 backgroundColor: "#EA4335",
+//                 data: [5000, 4500, 4000, 3500, 3000, 2500, 2000, 1500, 1000, 500],
+//             },
+//             {
+//                 // label: 'hoa cải',
+//                 backgroundColor: "#916BFF",
+//                 data: [5000, 4500, 4000, 3500, 3000, 2500, 2000, 1500, 1000, 500],
+//             },
+
+//         ],
+//     },
+//     options: {
+//         plugins: {
+//             legend: {
+//                 display: false,
+//             },
+//             datalabels: {
+//                 color: 'black',
+//                 align: 'end',
+//                 anchor: 'end',
+//                 formatter: function (value, context) {
+//                     return value; // Display the value as the label
+//                 }
+//             }
+//         },
+//         tooltips: {
+//             displayColors: true,
+//             callbacks: {
+//                 mode: 'x',
+//             },
+//         },
+//         scales: {
+//             x: {
+//                 stacked: true,
+//                 ticks: {
+//                     stepSize: 2500
+//                 },
+//             },
+//             y: {
+//                 stacked: true,
+//             }
+//         },
+//         indexAxis: 'y',
+//         responsive: true,
+//         categoryPercentage: 0.9,
+//         barPercentage: 0.7,
+//     },
+// });
+
+
+
+// ! @@@@@@@@@@@@@@@@@@@  Stacked Chart @@@@@@@@@@
+var options = {
+    series: [
+        {
+            name: 'Facebook',
+            data: [4000, 2500, 2000, 1500, 1932, 500, 1808, 500, 1491, 862],
+            // color: '#4599E5'
+        },
+        {
+            name: 'Tiktok',
+            data: [4000, 2500, 2000, 1500, 1932, 500, 1809, 500, 1492, 863],
+            // color: '#BD00FF'
+        },
+        {
+            name: 'Youtube',
+            data: [4000, 2500, 2000, 1500, 1932, 500, 1808, 500, 1491, 862],
+            // color: '#EA4335'
+        },
+        {
+            name: 'News',
+            data: [4666, 5950, 5000, 5556, 1932, 6020, 1809, 5420, 1492, 863],
+            // color: '#916BFF'
+        }
+    ],
+    chart: {
+        type: 'bar',
+        height: 350,
+        stacked: true,
+        toolbar: {
+            show: true
+        },
+        zoom: {
+            enabled: true
+        }
+    },
+    colors: ['#4599E5', '#BD00FF', '#EA4335', '#916BFF'],
+    dataLabels: {
+        display: false,
+        style: {
+            colors: ['#000']
+        }
+    },
+    responsive: [{
+        breakpoint: 480,
+        options: {
+            legend: {
+                position: 'bottom',
+                offsetX: -10,
+                offsetY: 0
+            }
+        }
+    }],
+    // plotOptions: {
+    //     bar: {
+    //         horizontal: true,
+    //         borderRadius: 0,
+    //         barHeight: '70%',
+    //     },
+    // },
+    plotOptions: {
+        bar: {
+            horizontal: true,
+            barHeight: '70%',
+            dataLabels: {
+                total: {
+                    enabled: true,
+                    offsetX: 10,
+                    offsetY: 9,
+                    style: {
+                        fontSize: '14px',
+                        fontWeight: 400,
+                    }
+                }
+            }
+        },
+    },
+    xaxis: {
+        categories: ["cần sa ", "bóng cười", "ma tuý", "súng lục", "hoa cải", "tem giấy", "bột trắng", "AK47", "ketamine", "THC"],
+        labels: {
+            formatter: function (val) {
+                return val + "K"
+            }
+        }
+    },
+    yaxis: {
+        max: 18000,
+        labels: {
+            show: false,
+            trim: false
+        }
+    },
+    dataLabels: {
+        enabled: false
+    },
+    legend: {
+        display: false,
+    },
+    toolbar: {
+        show: false,
+        offsetX: 0,
+        offsetY: 0,
+    },
+    fill: {
+        opacity: 1
+    }
+};
+
+var chart = new ApexCharts(document.querySelector("#chart"), options);
+chart.render();
+
+
+
---
+++ assets/js/custom.js
@@ -0,0 +1,161 @@
+
+       // *** Category Select ****
+       $(document).ready(function () {
+        // Initialize Select2
+        $(".mySelect").select2({
+        placeholder: "categories to filter",
+        // allowClear: true
+        });
+       // Function to add a selected item
+        function addSelectedItem(selectedOption) {
+            var selectedItem = $("<div>").addClass("selected-item");        
+            selectedItem.text(selectedOption.text);
+            var closeIcon = $("<span>").addClass("close-icon").text("×");
+    
+            closeIcon.on("click", function () {
+                selectedItem.remove();
+                $(".mySelect option[value='" + selectedOption.id + "']").prop("selected", false);
+                $(".mySelect").trigger("change");
+            });
+    
+            selectedItem.append(closeIcon);
+            $(".selectedItems").append(selectedItem);
+        }
+    
+        // Handle item selection
+        $(".mySelect").on("select2:select", function (e) {
+            var selectedOption = e.params.data;
+            addSelectedItem(selectedOption);
+        });
+    
+        // Handle item deselection
+        $(".mySelect").on("select2:unselect", function (e) {
+            var selectedOption = e.params.data;
+            $(".selected-item").each(function () {
+                if ($(this).text() === selectedOption.text) {
+                    $(this).remove();
+                }
+            });
+        });
+        $(".mySelect").on("select2:unselect", function (e) {
+            var selectedOption = e.params.data;
+            $(".selected-item").each(function () {
+                if ($(this).find('.close-icon').length > 0) {
+                    var textWithoutCloseIcon = $(this).clone().children().remove().end().text().trim();
+                    if (textWithoutCloseIcon === selectedOption.text) {
+                        $(this).remove();
+                    }
+                }
+            });
+        });
+    });
+    //  ** Location Select *****
+    $(document).ready(function () {
+        // Initialize Select2
+        $(".locationSelect").select2({
+        placeholder: "location to filter ",
+        // allowClear: true
+        });
+       // Function to add a selected item
+        function addSelectedItem(selectedOption) {
+            var selectedItem = $("<div>").addClass("selected-item");        
+            selectedItem.text(selectedOption.text);
+            var closeIcon = $("<span>").addClass("close-icon").text("×");
+    
+            closeIcon.on("click", function () {
+                selectedItem.remove();
+                $(".locationSelect option[value='" + selectedOption.id + "']").prop("selected", false);
+                $(".locationSelect").trigger("change");
+            });
+    
+            selectedItem.append(closeIcon);
+            $(".location_selectedItems").append(selectedItem);
+        }
+    
+        // Handle item selection
+        $(".locationSelect").on("select2:select", function (e) {
+            var selectedOption = e.params.data;
+            addSelectedItem(selectedOption);
+        });
+    
+        // Handle item deselection
+        $(".locationSelect").on("select2:unselect", function (e) {
+            var selectedOption = e.params.data;
+            $(".selected-item").each(function () {
+                if ($(this).text() === selectedOption.text) {
+                    $(this).remove();
+                }
+            });
+        });
+        $(".locationSelect").on("select2:unselect", function (e) {
+            var selectedOption = e.params.data;
+            $(".selected-item").each(function () {
+                if ($(this).find('.close-icon').length > 0) {
+                    var textWithoutCloseIcon = $(this).clone().children().remove().end().text().trim();
+                    if (textWithoutCloseIcon === selectedOption.text) {
+                        $(this).remove();
+                    }
+                }
+            });
+        });
+    });
+
+
+// ***************** [ select key  tag ] *****
+const ul = document.querySelector(".keygroup"),
+input = document.querySelector(".input_keys"),
+tagNumb = document.querySelector(".details span");
+
+let maxTags = 10,
+tags = ["AK-{d}", "AR-{d}", "AN-{d}", "AUG-{d}", "M16", "handgun", "pistol", "grenade", "airfield", "knife", "machete", "artillery", "Minefield",
+"barricade", "explosion", "battlefield", "breach", "booby trap", "bullet", "bulletproof", "capture", "captive", "hostage", "firearm", "biology weaponry", "chemical weaponry", "antiaircraft gun",
+"Big bertha", "B52", "german 88", "panzerfaust", "gun", "anthrax", "hydrogen cyanide", "yellow rain", "smallpox", "adamsite", "disphogene", "lewisite", "nervegas", "phosgene",
+"tear gas", "cloropirin", "flamesite", "flamethrower", "greek fire", "nuke", "rocket", "cloropirin",];
+
+countTags();
+createTag();
+
+function countTags(){
+    input.focus();
+    tagNumb.innerText = maxTags - tags.length;
+}
+
+function createTag(){
+    ul.querySelectorAll("li").forEach(li => li.remove());
+    tags.slice().reverse().forEach(tag =>{
+        let liTag = `<li>${tag} <i class="uit uit-multiply" onclick="remove(this, '${tag}')"></i></li>`;
+        ul.insertAdjacentHTML("afterbegin", liTag);
+    });
+    countTags();
+}
+
+function remove(element, tag){
+    let index  = tags.indexOf(tag);
+    tags = [...tags.slice(0, index), ...tags.slice(index + 1)];
+    element.parentElement.remove();
+    countTags();
+}
+
+function addTag(e){
+    if(e.key == "Enter"){
+        let tag = e.target.value.replace(/\s+/g, ' ');
+        if(tag.length > 1 && !tags.includes(tag)){
+            if(tags.length < 100000){
+                tag.split(',').forEach(tag => {
+                    tags.push(tag);
+                    createTag();
+                });
+            }
+        }
+        e.target.value = "";
+    }
+}
+
+input.addEventListener("keyup", addTag);
+
+const removeBtn = document.querySelector(".details button");
+removeBtn.addEventListener("click", () =>{
+    tags.length = 0;
+    ul.querySelectorAll("li").forEach(li => li.remove());
+    countTags();
+});
