eccc4c4b3af08a0cb3cb2e0dc8466d55e33aec6c
ajay5776 <144900898+ajay5776@users.noreply.github.com>
2023-10-11 16:08:42 +0530

    update changes

--- assets/js/map.js
+++ assets/js/map.js
@@ -14,7 +14,7 @@ $(document).ready(function() {
     //     $ellipses.attr('fill', '#f00');
     // });
     $('div.overview_image_container svg g.circle_click_cont').on('click', function() {
-        console.log("sdfjsdfl");
+        
         if($('.overview_map_line').hasClass('popupopn') && $('.overview_map_arrow').hasClass('popupopn')){
             $('.overview_map_line').removeClass('popupopn');
             $('.overview_map_arrow').removeClass('popupopn')
@@ -24,8 +24,17 @@ $(document).ready(function() {
             $('.overview_map_popupdata').addClass('popupopn');
         }
     });
+
+    
+$('.userDistentCard .listItem').on('click', function() {
+    console.log("helkli working");
+    const $listitem = $(this);
+    var list_data_id = $listitem.data('id');    
+    $('#svg_fb_geomap g.node2[id="'+list_data_id+'"]').click();
+});
+var glo_counter = 0;
 $('.node2').on('click', function() {
-    console.log("hellosdflkjasoidfj");
+    
     const $parent = $(this); // Get the parent 'g' element    
 
     var location = $parent.data('location');
@@ -49,17 +58,27 @@ $('.node2').on('click', function() {
     var age = $parent.data('age');
     var html_geo_info = '<div class="row gap-3"><div class="col-12"><div class="postHeading">Post location</div><div class="row gap-1"><div class="col-12"><div class="row align-items-center gx-2"><div class="col-5 leftHeading">Location</div><div class="col-7 sourceValue">'+location+'</div></div></div><div class="col-12"><div class="row align-items-center gx-2"><div class="col-5 leftHeading">District</div><div class="col-7 sourceValue">'+district+'</div></div></div><div class="col-12"><div class="row align-items-center gx-2"><div class="col-5 leftHeading">County</div><div class="col-7 sourceValue">'+county+'</div></div></div><div class="col-12"><div class="row align-items-center gx-2"><div class="col-5 leftHeading">City</div><div class="col-7 sourceValue">'+city+'</div></div></div><div class="col-12"><div class="row align-items-center gx-2"><div class="col-5 leftHeading">Country</div><div class="col-7 sourceValue">'+country+'</div></div></div></div></div><div class="col-12"><div class="postHeading">Post timestamp</div><div class="row gap-1"><div class="col-12"><div class="row align-items-center gx-2"><div class="col-5 leftHeading">Day</div><div class="col-7 sourceValue">'+day+'</div></div></div><div class="col-12"><div class="row align-items-center gx-2"><div class="col-5 leftHeading">Time</div><div class="col-7 sourceValue">'+time+'</div></div></div></div></div><div class="col-12"><div class="postHeading">More details</div><div class="row gap-1"><div class="col-12"><div class="row align-items-center gx-2"><div class="col-5 leftHeading">Devices</div><div class="col-7 sourceValue">'+devices+'</div></div></div><div class="col-12"><div class="row align-items-center gx-2"><div class="col-5 leftHeading">Wifi ID</div><div class="col-7 sourceValue">'+wifiid+'</div></div></div><div class="col-12"><div class="row align-items-center gx-2"><div class="col-5 leftHeading">MAC</div><div class="col-7 sourceValue">'+mac+'</div></div></div><div class="col-12"><div class="row align-items-center gx-2"><div class="col-5 leftHeading">IP address</div><div class="col-7 sourceValue">'+ipaddress+'</div></div></div><div class="col-12"><div class="row align-items-center gx-2"><div class="col-5 leftHeading">Port</div><div class="col-7 sourceValue">'+port+'</div></div></div></div></div><div class="col-12"><div class="postHeading">Account info</div><div class="row gap-1"><div class="col-12"><div class="row align-items-center gx-2"><div class="col-5 leftHeading">Account</div><div class="col-7 sourceValue">'+account+'</div></div></div><div class="col-12"><div class="row align-items-center gx-2"><div class="col-5 leftHeading">Followers</div><div class="col-7 sourceValue">'+followers+'</div></div></div><div class="col-12"><div class="row align-items-center gx-2"><div class="col-5 leftHeading">Posts</div><div class="col-7 sourceValue">'+posts+'</div></div></div><div class="col-12"><div class="row align-items-center gx-2"><div class="col-5 leftHeading">Likes</div><div class="col-7 sourceValue">'+likes+'</div></div></div><div class="col-12"><div class="row align-items-center gx-2"><div class="col-5 leftHeading">Friends</div><div class="col-7 sourceValue">'+friends+'</div></div></div><div class="col-12"><div class="row align-items-center gx-2"><div class="col-5 leftHeading">Mobile</div><div class="col-7 sourceValue">'+mobile+'</div></div></div><div class="col-12"><div class="row align-items-center gx-2"><div class="col-5 leftHeading">Age</div><div class="col-7 sourceValue">'+age+'</div></div></div></div></div></div>';
     // Loop through each 'ellipse' element
+
     $parent.each(function() {
         const $ellipse = $(this);
-        if ($parent.hasClass('animation_color')) {            
-            $parent.removeClass('animation_color');
+
+        var map_profile_id  = $parent.attr('id');
+        if ($parent.hasClass('animation_color')) {                  
+            $parent.removeClass('animation_color');                              
+            $('.userDistentCard .listItem[data-id="'+map_profile_id+'"]').removeClass('active');
+            glo_counter--;
         } else {            
-            $parent.addClass('animation_color');
-            $('.popupdata').show();
-            console.log("Hello");
+            $parent.addClass('animation_color');            
+            $('.userDistentCard .listItem[data-id="'+map_profile_id+'"]').addClass('active');
+            glo_counter++;
+            $('.popupdata').show();            
             $('.geoInfoCard .card-body > div').html(html_geo_info);                    
         }
     });
+    console.log("counter", glo_counter);
+    if(glo_counter == 0) {
+        $('.popupdata').hide();
+    }
 });
 
 });
